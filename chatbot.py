8162f96d6a158cb1c03863342498507e6e6ffd0a
sripatlu-sudo <sripatlu@gmail.com>
2025-11-26 09:46:54 -0600

    tet

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
 
-with st.chat_message("user"):
-    st.write("Hello!")
+with st.chat_message("assistant"):
+    st.write("Hello Human!")
+    st.bar_chart(np.random.randn(30,3))
