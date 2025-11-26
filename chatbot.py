b77f2d3f8ff2b1ab438a8e86765c28345ab04229
sripatlu-sudo <sripatlu@gmail.com>
2025-11-26 10:18:35 -0600

    wefrewrew

--- chatbot.py
+++ chatbot.py
@@ -32,13 +32,13 @@ def response_generator():
                 "Hello there! How can I assist you today?",
                 "Hi, human! Is there anything I can help you with?",
                 "Do you need help?"
-
             ]
 
-            for word in response.split():
-                 yield word + " "
-            time.sleep(0.05)
+         
     )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
 
 # Display assistant response in chat message container
