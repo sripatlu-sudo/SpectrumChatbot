7919d5faf40d12d3f58f47d506c0ee915efa09d8
sripatlu-sudo <sripatlu@gmail.com>
2025-11-26 09:49:20 -0600

    dfdsafd

--- chatbot.py
+++ chatbot.py
@@ -4,3 +4,7 @@ import numpy as np
 with st.chat_message("assistant"):
     st.write("Hello Human!")
     st.bar_chart(np.random.randn(30,3))
+
+prompt = st.chat_input("Say something...")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
