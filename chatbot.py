96331efee9ce06f4d142865ac7ead2b71b2343bf
sripatlu-sudo <sripatlu@gmail.com>
2025-11-26 10:01:45 -0600

    rtrt

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,6 @@
 import streamlit as st
 #import numpy as np
 
-"""
-with st.chat_message("assistant"):
-    st.write("Hello Human!")
-    st.bar_chart(np.random.randn(30,3))
-
-prompt = st.chat_input("Say something...")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
-"""
-
 st.title("Echo bot")
 
 # Initialize chat history
