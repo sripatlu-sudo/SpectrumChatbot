05a2007dd4d30fdf3ca35bac72e65fdea789a0ce
sripatlu-sudo <sripatlu@gmail.com>
2025-11-26 10:16:50 -0600

    rewrewr

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import random
+import time
 #import numpy as np
 
-st.title("Echo bot")
+st.title("Simple Chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -21,9 +23,26 @@ if prompt := st.chat_input("What is up?"):
 
         st.session_state.messages.append({"role":"user","content":prompt})
 
-        response = f"Echo: {prompt}"
 
-        with st.chat_message("assistant"):
-            st.markdown(response)
-        
-        st.session_state.messages.append({"role":"assistant","content":response})
+
+#Streamed response emulator
+def response_generator():
+    response = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Hi, human! Is there anything I can help you with?",
+                "Do you need help?"
+
+            ]
+
+            for word in response.split():
+                 yield word + " "
+            time.sleep(0.05)
+    )
+
+
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    response = st.write_stream(response_generator())
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
