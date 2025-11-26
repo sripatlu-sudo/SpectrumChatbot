6b2d59c4cf1f5af71cbbbdd675f6bbe4db29d8ba
sripatlu-sudo <sripatlu@gmail.com>
2025-11-26 11:06:02 -0600

    checkin

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,11 @@
 
-import streamlit as st
+import pathlib, streamlit as st
 from openai import OpenAI
+from langchain_classic.vectorstores import FAISS
+from langchain_classic.embeddings import HuggingFaceEmbeddings
+from langchain_classic.llms import Ollama
+from langchain_classic.chains import ConversationalRetrievalChain
+from langchain_classic.memory import ConversationBufferMemory
 
 st.title("ChatGPT-like clone!")
 
@@ -32,3 +37,27 @@ if prompt := st.chat_input("What is up?"):
         )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+    
+@st.cache_resource
+def init_chain():
+    vectordb = FAISS.load_local(
+        "faiss_index",
+        HuggingFaceEmbeddings(model_name="thenlper/gte-small"),
+        allow_dangerous_deserialization=True,
+    )
+    retriever = vectordb.as_retriever(search_kwargs={"k": 8})
+    llm = Ollama(model="gemma3:1b", temperature=0.1)
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True,
+    )
+
+    return ConversationalRetrievalChain.from_llm(
+        llm,
+        retriever,
+        memory=memory,
+    )
+
+chain = init_chain()
