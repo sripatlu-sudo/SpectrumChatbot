549f90b32c99525c86621383342ad9cc95ff708b
sripatlu-sudo <sripatlu@gmail.com>
2025-11-26 10:19:28 -0600

    hytrytr

--- chatbot.py
+++ chatbot.py
@@ -38,7 +38,7 @@ def response_generator():
     )
     for word in response.split():
         yield word + " "
-        time.sleep(0.05)
+        time.sleep(5)
 
 
 # Display assistant response in chat message container
